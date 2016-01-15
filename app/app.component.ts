e408c4eff86cd0d55c5a502d3fb3914686082e63
Shawn Saras <ssaras@enclos.com>
2016-01-14 18:49:26 -0800

    started hero app thingy

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,33 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>My First Angular 2 App!</h1>'
+	styleUrls: ['app/templates/styles/appComponent.css'],
+	templateUrl: 'app/templates/appComponent.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+	public title = 'Tour of Heroes';
+	public hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	}
+	public heroes = HEROES;
+}
+
+interface Hero {
+	id: number;
+	name: string;
+}
+
+var HEROES: Hero[] = [
+	{ 'id': 11, 'name': 'Mr. Nice' },
+	{ 'id': 12, 'name': 'Narco' },
+	{ 'id': 13, 'name': 'Bombasto' },
+	{ 'id': 14, 'name': 'Celeritas' },
+	{ 'id': 15, 'name': 'Magneta' },
+	{ 'id': 16, 'name': 'RubberMan' },
+	{ 'id': 17, 'name': 'Dynama' },
+	{ 'id': 18, 'name': 'Dr IQ' },
+	{ 'id': 19, 'name': 'Magma' },
+	{ 'id': 20, 'name': 'Tornado' }
+];
